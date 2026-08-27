715868db8b504cc989d138912c25e553e36828bb
LorenRF <20220098@itla.edu.do>
2026-08-27 05:11:05 -0400

    fix(frontend): remove default Angular welcome page and connect app layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,11 +6,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent],
-  template: `
-    <app-navbar></app-navbar>
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-  `
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {}
